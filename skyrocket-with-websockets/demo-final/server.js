a9813789842930552a20475156f64bf143dad685
aaron <alkrauss48@gmail.com>
2016-05-13 21:36:50 -0500

    Set up demo and add demo-final

---
+++ skyrocket-with-websockets/demo-final/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+
+app.use(express.static(__dirname + '/public'));
+
+var io = require('socket.io')(server);
+io.on('connection', function(socket){
+
+  socket.on('message', function(data){
+    console.log(data);
+
+    socket.broadcast.emit('message', {
+      message: data
+    });
+  });
+
+});
+
+server.listen(3000);
