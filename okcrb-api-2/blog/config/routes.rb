bd8fb31154fd8e3c04598a83381dd9c70b9767aa
aaron <alkrauss48@gmail.com>
2017-01-24 23:06:39 -0600

    Finish test blog and add files

--- okcrb-api-2/blog/config/routes.rb
+++ okcrb-api-2/blog/config/routes.rb
@@ -1,6 +1,9 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    resources :comments
+    resources :posts
+    resources :users
+  end
 end
