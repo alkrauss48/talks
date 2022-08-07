1a81cbf6e478c2e2e7a78a0653b20b09ec2abff5
aaron <alkrauss48@gmail.com>
2022-08-07 13:51:57 -0500

    Remove js-testing-and-refactoring dir in favor of okcwebdevs repo

---
+++ js-testing-and-refactoring/README.md
@@ -0,0 +1,3 @@
+# JS Testing and Refactoring
+
+[See repo on OKC WebDevs Github Organization](https://github.com/OkcWebDevs/js-testing-and-refactoring)
--- js-testing-and-refactoring/after/components/GenerateTotalPrice.test.js
+++
@@ -1,7 +0,0 @@
-import generateTotalPrice from './GenerateTotalPrice.js';
-
-test('properly generates total price', () => {
-  const input = [1, 2];
-
-  expect(generateTotalPrice(input)).toBe(3.24);
-});
